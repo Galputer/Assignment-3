c43c339f26ad6dc4f5ebc3f46994807255cb123f
Galputer <eakelley2001@gmail.com>
2023-04-03 19:17:42 -0400

    parsed csv boards into pandas
    Iterated through entire list of boards and formatted them to separate the astar value and board start state. Also updated .gitignore so that data set doesn't get added to the repo

--- A_Star/ML.py
+++ A_Star/ML.py
@@ -1,3 +1,6 @@
+import os
+import pandas as pd
+import numpy as np
 # we don't need iris datasets, we have our own
 #from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
@@ -5,11 +8,25 @@ from sklearn.model_selection import train_test_split
 
 #starting file for machine learning new permissable heuristic
 def ml(args):
-
+    directory = 'board-data'
+    testData = pd.DataFrame()
+    
     # Load the a different dataset based on our boardsS
     ## we need a panda dataset, input from the Main.py script
-    iris = load_iris()
-    X, y = iris.data, iris.target
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            df = pd.read_csv(f)
+            boardnums = df.to_numpy()
+            ace_calc = boardnums[len(boardnums)-1]
+            boardnums = np.delete(boardnums, -1)
+            tempDF = pd.DataFrame({'matrix': boardnums, 'astar_calc':ace_calc})
+            testData.append(tempDF, ignore_index=True)
+        
+    X, y = testData.data, testData.target
 
     # Split the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
