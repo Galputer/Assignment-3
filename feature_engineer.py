b7463458ef4958ca8acb4e1bf24fc99a29c69c5b
Shundong Li <61202251+YamaFuso@users.noreply.github.com>
2023-04-08 14:12:03 -0400

    naive nn (#3)
    * naive neural network implementation
    
    * add inversion count to features

--- feature_engineer.py
+++ feature_engineer.py
@@ -80,6 +80,28 @@ def linear_conflict(board, goal, n):
                         conflicts += value + other_tile
     return conflicts
 
+def inversions_count_1(board):
+    """
+    Calculates the number of inversions in the given board state.
+    """
+    inversions = 0
+    for i in range(len(board)):
+        for j in range(i + 1, len(board)):
+            if board[i] != "B" and board[j] != "B" and board[i] > board[j]:
+                inversions += 1
+    return inversions
+
+def inversions_count_2(board):
+    """
+    Calculates the number of inversions in the given board state.
+    """
+    inversions = 0
+    for i in range(len(board)):
+        for j in range(i + 1, len(board)):
+            if board[i] != "B" and board[j] != "B" and board[i] < board[j]:
+                inversions += 1
+    return inversions
+
 def feature_engineer(df: pd.DataFrame):
     df['board_size'] = df['board'].apply(len)
     df['n'] = df['board_size'].apply(isqrt)
@@ -99,10 +121,14 @@ def feature_engineer(df: pd.DataFrame):
     df['conflicts_1'] = df.apply(lambda x: linear_conflict(x.board, x.goal_1, x.n) + x.manhattan_1, axis=1)
     df['conflicts_2'] = df.apply(lambda x: linear_conflict(x.board, x.goal_2, x.n) + x.manhattan_2, axis=1)
     df['conflicts'] = df.apply(lambda x: min(x.conflicts_1, x.conflicts_2), axis=1)
+    df['inversion_1'] = df.apply(lambda x: inversions_count_1(x.board), axis=1)
+    df['inversion_2'] = df.apply(lambda x: inversions_count_2(x.board), axis=1)
+    df['inversion'] = df.apply(lambda x: min(x.inversion_1, x.inversion_2), axis=1)
     
     
     # return df[['tile_sum','correct_1','correct_2','correct_count','incorrect_sum_1','incorrect_sum_2','incorrect_sum','manhattan_1','manhattan_2','manhattan','conflicts_1','conflicts_2','conflicts']]
-    return df[['tile_sum','correct_count','incorrect_sum','manhattan','conflicts']]
+    return df[['tile_sum','correct_count','inversion','incorrect_sum','manhattan','conflicts']]
+
 if __name__ == "__main__":
     # board_1d = np.array(['B', 'B', 5, 3, 10, 6, 11, 7, 4, 8, 9, 16, 12, 'B', 13, 15])
     # board_sorted = [3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 15, 16]
